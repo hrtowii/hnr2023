c16c4bc5e36d4c2724426e642256aaf2dfca8bfb
Raymond <raymond.goo124@gmail.com>
2023-01-14 19:09:06 +0800

    App.tsx: Use CoffeeContext provider.

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,22 +1,42 @@
-import {Card, Text} from "@ui-kitten/components";
-import {View, StyleSheet} from "react-native";
-import {Coffee} from "../utils/utils";
+import { Card, Text } from "@ui-kitten/components";
+import { View, StyleSheet } from "react-native";
+import { Coffee } from "../utils/utils";
+import { useContext } from "react";
+import { CoffeeContext } from "../context/CoffeeContext";
 
 interface Props {
-    coffee: Coffee
+	coffee: Coffee;
+	navigation: any;
+	setCoffee: (coffee: Coffee) => void;
 }
 
 export const MenuCard = (props: Props) => {
-    return (
-            <Card style={styles.card} onPress={() => {}}>
-                <Text category="h3">{props.coffee.name}</Text>
-            </Card>
-    )
-}
+	const coffee = useContext(CoffeeContext);
+	return (
+		<Card
+			style={styles.card}
+			onPress={() => {
+				props.setCoffee(props.coffee);
+				props.navigation.navigate("Ratio");
+				console.log(coffee);
+			}}
+		>
+			<Text category="h3">{props.coffee.name}</Text>
+			<View
+				style={{
+					padding: 80,
+					borderRadius: 8,
+					marginTop: 10,
+					backgroundColor: "#ecf0f1",
+				}}
+			></View>
+		</Card>
+	);
+};
 
 const styles = StyleSheet.create({
-    card: {
-        borderRadius: 8,
-        width: "50%"
-    }
-})
+	card: {
+		borderRadius: 8,
+		width: "50%",
+	},
+});
--- pages/Ratio.tsx
+++ pages/Ratio.tsx
@@ -1,33 +1,41 @@
-import {  View, StyleSheet } from 'react-native';
-import {useState} from 'react';
-import Constants from 'expo-constants';
-import { ApplicationProvider, Layout, Text, Button } from '@ui-kitten/components';
+import { View, StyleSheet } from "react-native";
+import { useContext, useState } from "react";
+import Constants from "expo-constants";
+import {
+	ApplicationProvider,
+	Layout,
+	Text,
+	Button,
+} from "@ui-kitten/components";
+import { CoffeeContext } from "../components/context/CoffeeContext";
 
-export default function Ratio({navigation, route}){
-    const recipe = {}//route.params.recipe;
-    const [ratio, setRatio] = useState({})
-    
-    function moveOn(){
-        navigation.navigate("Timer", {recipe,ratio})
-    }
-    return <View>
-      <Button>Configure</Button>
-    </View>
+export default function Ratio({ navigation, route }) {
+	const coffee = useContext(CoffeeContext);
+	const [ratio, setRatio] = useState({});
+
+	function moveOn() {
+		// navigation.navigate("Timer", {recipe,ratio})
+	}
+	return (
+		<View>
+			<Text>Current coffee: {coffee.name}</Text>
+			<Button>Configure</Button>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		paddingTop: Constants.statusBarHeight,
+		backgroundColor: "#ecf0f1",
+		padding: 8,
+	},
+	paragraph: {
+		margin: 24,
+		fontSize: 18,
+		fontWeight: "bold",
+		textAlign: "center",
+	},
 });
-
